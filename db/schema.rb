a85b29135d345e014dc819527430babc4a310855
BilionBoy <dhiogoandre26@gmail.com>
2025-01-03 12:53:55 -0400

    Feat: Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_03_165252) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_03_165331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.bigint "establishment_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["establishment_id"], name: "index_categories_on_establishment_id"
+  end
 
   create_table "establishments", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -39,5 +47,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_03_165252) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "establishments"
   add_foreign_key "establishments", "users"
 end
